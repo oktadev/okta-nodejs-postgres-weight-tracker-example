de9cfddef6e68c6069cc2fbe95cb09ac4a5416b1
Elina <elina2412@yahoo.com>
2022-05-19 11:40:29 -0400

    new branch  for deploying app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const createServer = require( "./server" ).createServer;
 const init = async () => {
 	dotenv.config();
 	const config = {
-		port: process.env.PORT || 3000,
+		port: process.env.PORT || 8080,//My edit from 3000 to 8080
 		host: process.env.HOST || "localhost"
 	};
 	const server = await createServer( config );
